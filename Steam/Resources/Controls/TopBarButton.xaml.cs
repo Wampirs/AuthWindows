d7b533f938a3f1bcbfb593cded0a5c625cb4a7fb
Wampirs <79167501+Wampirs@users.noreply.github.com>
2022-07-13 21:10:45 +0300

    Creeated TopBarButtons

---
+++ Steam/Resources/Controls/TopBarButton.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AuthWindows.Steam.Resources.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для TopBarButton.xaml
+    /// </summary>
+    public partial class TopBarButton : UserControl
+    {
+        public TopBarButton()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public static readonly DependencyProperty SourceImageProperty = DependencyProperty.Register(
+            name: "SourceImage",
+            propertyType: typeof(ImageSource),
+            ownerType: typeof(TopBarButton),
+            typeMetadata: new FrameworkPropertyMetadata(
+                defaultValue: null,
+                flags: FrameworkPropertyMetadataOptions.AffectsRender,
+                propertyChangedCallback: null));
+
+        public ImageSource SourceImage
+        {
+            get { return (ImageSource)GetValue(SourceImageProperty); }
+            set { SetValue(SourceImageProperty, value); }
+        }
+
+
+    }
+}
